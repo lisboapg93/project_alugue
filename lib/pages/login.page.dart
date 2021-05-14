987bf5ec78dbd78e9ba84771add4c0551f6b51a6
lisboapg93 <pedrogabriel3@hotmail.com>
2021-05-14 04:00:22 -0300

    upgrade

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -5,7 +5,11 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40
+        ),
         color: Colors.white,
         child: ListView(
           children: <Widget>[
@@ -22,11 +26,14 @@ class LoginPage extends StatelessWidget {
               decoration: InputDecoration(
                 labelText: "email",
                 labelStyle: TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20),
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20
+                ),
               ),
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(
+                fontSize: 20
+              ),
             ),
             SizedBox(
               height: 10,
@@ -37,11 +44,14 @@ class LoginPage extends StatelessWidget {
               decoration: InputDecoration(
                 labelText: "senha",
                 labelStyle: TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20),
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20
+                ),
               ),
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(
+                fontSize: 20
+              ),
             ),
             Container(
               height: 40,
@@ -58,7 +68,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Container(
-              height: 60,
+              height: 65,
               alignment: Alignment.centerLeft,
               decoration: BoxDecoration(
                 gradient: LinearGradient(
@@ -71,29 +81,58 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
                 borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+                  Radius.circular(20),
                 ),
               ),
               child: SizedBox.expand(
                 child: FlatButton(
                   child: Row(
-                    children: <Widget>[
-                      Text(
-                        "Login",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        // textAlign: TextAlign.center,
+                    children: <Widget> [
+                      Text("Login",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                      // textAlign: TextAlign.center,
                       ),
                     ],
                   ),
                   onPressed: () => {},
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Colors.indigo.shade900,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              // child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    children: <Widget> [
+                      Text("Cadastrar-se",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                      // textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  onPressed: () => {},
+                ),
+            ),
           ],
         ),
       ),
